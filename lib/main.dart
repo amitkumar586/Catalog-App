10c5012c519668266b9348a3eb86896790feb0ec
Amit Kumar <amitkumargangwar189@gmail.com>
2023-05-26 16:24:17 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CatalogApp extends StatefulWidget {
+  const CatalogApp({super.key});
+
+  @override
+  State<CatalogApp> createState() => _CatalogAppState();
+}
+
+class _CatalogAppState extends State<CatalogApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Catalog App",
+    );
+  }
+}
